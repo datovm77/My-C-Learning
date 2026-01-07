bb80d2eb742b0c5b1175e7064470bfc804c903ec
datovm <datou.123321@qq.com>
2026-01-07 15:36:56 +0800

    错误演示—汉字的处理

---
+++ C _learning/learning-163.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    getchar();
+    for(int i = 0;i<n;i++)
+    {
+        char str[1000];
+        int len;
+        if(fgets(str,sizeof(str),stdin)!=NULL)
+        {
+            len = strlen(str);
+            if(len>0&&str[len-1]=='\n')
+            {
+                str[len-1] = '\0';
+                len--;
+            }
+            int count = 0;
+            for(int i = 0;i<len;i++)
+            {
+                if(isascii(str[i]))
+                {
+                    count++;
+                }
+            }
+            int han = (len-count)/3;
+            printf("%d\n",han);
+        }
+    }
+
+
+    return 0;
+}
